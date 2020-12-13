1e8c0118df2dd5c6d45d86c2a85e80ff7e3f0564
Bishal Neupane <neupanebishal07@gmail.com>
2020-12-13 16:23:31 +0545

    User Register & Password Hashing

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -47,4 +47,34 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile }
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userAlreadyExists = await User.findOne({ email })
+  if (userAlreadyExists) {
+    res.status(400)
+    throw new Error('User Already Exists')
+  }
+
+  const user = await User.create({ name, email, password })
+
+  if (user) {
+    res.status(201)
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
+
+export { authUser, getUserProfile, registerUser }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,6 +27,15 @@ const userSchema = mongoose.Schema(
   }
 )
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 userSchema.methods.matchPassword = async function (enterdPassword) {
   return await bcrypt.compare(enterdPassword, this.password)
 }
