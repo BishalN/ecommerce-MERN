7687f1787c9ebad3660c8f6e0b1690ed36397178
Bishal Neupane <neupanebishal07@gmail.com>
2020-12-15 16:58:18 +0545

    Create Order

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -20,11 +20,11 @@ const OrderSchema = mongoose.Schema(
         },
       },
     ],
-    shippingAddres: {
+    shippingAddress: {
       address: { type: String, required: true },
       city: { type: String, required: true },
-      state: { type: String, required: true },
       country: { type: String, required: true },
+      postalCode: { type: String, required: true },
     },
     paymentMethod: {
       type: String,
