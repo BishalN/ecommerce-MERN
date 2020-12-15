cf2235fb7d5eb6ce9244ae06a9cdb6a725542053
Bishal Neupane <neupanebishal07@gmail.com>
2020-12-15 09:26:39 +0545

    user register,reducer&action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
           <Route path='/product/:id' component={ProductScreen}></Route>
           <Route path='/cart/:id?' component={CartScreen}></Route>
           <Route path='/login' component={LoginScreen}></Route>
+          <Route path='/register' component={RegisterScreen}></Route>
         </Container>
       </main>
       <Footer />
