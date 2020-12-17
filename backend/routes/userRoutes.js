a43bdb6e57769b6b69046950e1a6515505672bc9
Bishal Neupane <neupanebishal07@gmail.com>
2020-12-17 09:52:10 +0545

    Get user by id and Update User endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,11 +8,17 @@ import {
   registerUser,
   getUsers,
   deleteUsers,
+  getUserById,
+  updateUser,
 } from '../controller/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(protect, admin, getUsers)
-router.route('/:id').delete(protect, admin, deleteUsers)
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUsers)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 router.route('/').post(registerUser)
 router.post('/login', authUser)
 router
