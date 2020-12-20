76a52c3d56b0ba63f862aaf909a2d1c593be6eff
Bishal Neupane <neupanebishal07@gmail.com>
2020-12-20 07:38:48 +0545

    Product Pagination

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -6,17 +6,24 @@ import Product from '../models/productModel.js'
 // @access  public
 
 const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 10
+  const page = Number(req.query.pageNumber) || 1
+
   const keyword = req.query.keyword
     ? {
         name: {
           $regex: req.query.keyword,
-          $otions: 'i',
+          $options: 'i',
         },
       }
     : {}
 
+  const count = await Product.countDocuments({ ...keyword })
+
   const products = await Product.find({ ...keyword })
-  res.json(products)
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
+  res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
 
 // @desc    Fetch a product by id
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,17 +5,19 @@ import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import { listProducts } from '../actions/productActions'
 import Loader from '../components/Loader'
+import Paginate from '../components/Paginate'
 
 const HomeScreen = ({ match }) => {
-  const keyword = match.parmas.keyword
+  const keyword = match.params.keyword
+  const pageNumber = match.params.pageNumber || 1
   const dispatch = useDispatch()
 
   const productList = useSelector((state) => state.productList)
-  const { error, loading, products } = productList
+  const { error, loading, products, page, pages } = productList
 
   useEffect(() => {
-    dispatch(listProducts(keyword))
-  }, [dispatch, keyword])
+    dispatch(listProducts(keyword, pageNumber))
+  }, [dispatch, keyword, pageNumber])
 
   return (
     <>
@@ -25,13 +27,20 @@ const HomeScreen = ({ match }) => {
       ) : error ? (
         <h3>{error}</h3>
       ) : (
-        <Row>
-          {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
+        <>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ''}
+          />
+        </>
       )}
     </>
   )
