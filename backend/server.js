e9b69e8c44b22ae02cb634f59e82136504f6d931
Bishal Neupane <neupanebishal07@gmail.com>
2020-12-21 10:27:16 +0545

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,16 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+app.use(express.json())
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID)
+})
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
@@ -35,18 +45,6 @@ if (process.env.NODE_ENV === 'production') {
     res.send('API is running....')
   })
 }
-
-app.use(express.json())
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
-app.use('/api/upload', uploadRoutes)
-
-app.get('/api/config/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID)
-})
-
-
 
 app.use(notFound)
 app.use(errorHandler)
